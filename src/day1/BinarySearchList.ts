1fc4a27d25c5e99e7b7f65d9860db4f693abc9ad
gabriel garcia <gabrielgarcia.contactme@gmail.com>
2024-10-18 21:11:57 -0300

    add binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  let left = 0
+  let right = haystack.length
 
+  while (left < right) {
+    let middle = Math.floor(left + (right - left) / 2)
+
+    if (haystack[middle] === needle) {
+      return true
+    } else if (needle > haystack[middle]) {
+      left = middle + 1
+    } else {
+      right = middle
+    }
+  }
+
+  return false
 }
