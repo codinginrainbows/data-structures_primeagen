5d27a146215487eac6983abe383403c15caaa6fb
gabriel garcia <gabrielgarcia.contactme@gmail.com>
2024-10-23 21:32:29 -0300

    add: doublyl linked list node

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,9 +1,22 @@
+class Node<T> {
+    public value: T;
+    public next: Node<T> | null = null;
+    public prev: Node<T> | null = null;
+
+    constructor(value: T) {
+        this.value = value;
+    }
+}
+
 export default class DoublyLinkedList<T> {
     public length: number;
-
-    
+    private head: Node<T> | null;
+    private tail: Node<T> | null;
 
     constructor() {
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
     }
 
     prepend(item: T): void {
